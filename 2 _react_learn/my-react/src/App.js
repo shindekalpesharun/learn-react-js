9737030aeabcc44442cb23515340cb7fcbb77a2a
shindekalpesharun <shindekalpesharun@gmail.com>
2021-10-18 16:32:40 +0530

    12 chrome dev tool for react by facebook

--- 2 _react_learn/my-react/src/App.js
+++ 2 _react_learn/my-react/src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            title: 'Hello react'
-        }
-    }
-    render() {
-        return <div>{this.state.title}</div>;
-    }
-}
-
-// function App() {
-//     return <div>Hello React</div>;
+// class App extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             title: 'Hello react'
+//         }
+//     }
+//     render() {
+//         return <div>{this.state.title}</div>;
+//     }
 // }
+
+function App({ title }) {
+    return <div>{title}</div>;
+}
 
 export default App;

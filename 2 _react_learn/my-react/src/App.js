ca8d65ddcf1d4cef9df97c00eb62af31b095eea9
shindekalpesharun <shindekalpesharun@gmail.com>
2021-11-13 10:17:53 +0530

    17 click toggle event

--- 2 _react_learn/my-react/src/App.js
+++ 2 _react_learn/my-react/src/App.js
@@ -1,25 +1,38 @@
 import React from "react";
 import "./assets/css/style.css"
 
-// class App extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             title: 'Hello react'
-//         }
-//     }
-//     render() {
-//         return <div>{this.state.title}</div>;
-//     }
-// }
-
-function App({ title }) {
-    return <div>
-        <div className="bg-gray-600 text-white p-5 border">
-            {title}
-        </div>
-    </div >;
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: 'Hello react 2',
+            isShowing: false,
+        }
+    }
+    render() {
+        return <section className="flex justify-center">
+            <div className="w-1/2">
+                <div className="my-4">{this.state.title}</div>
+                <div>
+                    <button className="p-1 bg-blue-700 text-white" onClick={() => (this.setState({ isShowing: !this.state.isShowing }))}>Toggle Image</button>
+                    {
+                        this.state.isShowing ?
+                            <img src="https://images.unsplash.com/photo-1636224358189-182d4886cf14?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8" />
+                            : null
+                    }
+                </div>
+            </div>
+        </section >;
+    }
 }
+
+// function App({ title }) {
+//     return <div>
+//         <div className="bg-gray-600 text-white p-5 border">
+//             {title}
+//         </div>
+//     </div >;
+// }
 
 
 

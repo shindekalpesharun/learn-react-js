84d61a0a39b799fe0b506750af9c4380642914fe
shindekalpesharun <shindekalpesharun@gmail.com>
2021-11-13 10:27:22 +0530

    18 bind this in function

--- 2 _react_learn/my-react/src/App.js
+++ 2 _react_learn/my-react/src/App.js
@@ -4,17 +4,27 @@ import "./assets/css/style.css"
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            title: 'Hello react 2',
-            isShowing: false,
-        }
+        this.state = { title: 'Hello react 2', isShowing: false }
+        // this.handleClick = this.handleClick.bind(this);
     }
+    // states are Immutable
+    // TODO: use always arrow function normal function this keyword not scope then bind 
+    handleClick = () => {
+        this.setState({ isShowing: !this.state.isShowing })
+    }
+
+    // handleClick() {
+    //     this.setState({ isShowing: !this.state.isShowing })
+    // }
+
+
+
     render() {
         return <section className="flex justify-center">
             <div className="w-1/2">
                 <div className="my-4">{this.state.title}</div>
                 <div>
-                    <button className="p-1 bg-blue-700 text-white" onClick={() => (this.setState({ isShowing: !this.state.isShowing }))}>Toggle Image</button>
+                    <button className="p-1 bg-blue-700 text-white" onClick={this.handleClick}>Toggle Image</button>
                     {
                         this.state.isShowing ?
                             <img src="https://images.unsplash.com/photo-1636224358189-182d4886cf14?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8" />

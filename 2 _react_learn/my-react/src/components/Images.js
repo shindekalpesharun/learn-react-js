44113b98e69aa2422d07cc3b52056293eef83f65
shindekalpesharun <shindekalpesharun@gmail.com>
2021-11-15 19:32:45 +0530

    24 component inside component map function

--- 2 _react_learn/my-react/src/components/Images.js
+++ 2 _react_learn/my-react/src/components/Images.js
@@ -1,48 +1,26 @@
 import React, { useEffect, useState } from 'react'
 
 export default function Images() {
-    const [myInterval, setmyInterval] = useState(null);
-    useEffect(() => {
-        console.log('Images Comp Mounted')
-        const interval = setInterval(() => {
-            console.log('Hello');
-        }, 1000);
+    const Images = [
+        'https://images.unsplash.com/photo-1636224358189-182d4886cf14?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8',
+        'https://images.unsplash.com/photo-1636934432265-7b770c648a4e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=805&q=80',
+        'https://images.unsplash.com/photo-1586227740560-8cf2732c1531?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=961&q=80',
 
-        return () => clearInterval(interval);
-    }, []);
+    ]
+
+    function ShowImage() {
+        return Images.map((image) => {
+            return (
+                <div>
+                    <img src={image} alt="error" />
+                </div>
+            );
+        });
+    }
 
     return (
-        <div>
-            <img src="https://images.unsplash.com/photo-1636224358189-182d4886cf14?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8" alt="error" />
-        </div>
+        <section>
+            <ShowImage />
+        </section>
     )
 }
-
-// export default class images extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = { interval: null }
-//     }
-
-//     componentDidMount() {
-//         console.log('Images comp Mounted');
-//         this.setState({
-//             interval: setInterval(() => {
-//                 console.log('Hello');
-//             }, 1000),
-//         })
-//     }
-
-//     componentWillUnmount() {
-//         console.log('Images Component unmounted')
-//         clearInterval(this.state.interval);
-//     }
-//     render() {
-//         return (
-//             <div>
-//                 <img src="https://images.unsplash.com/photo-1636224358189-182d4886cf14?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8" alt="error" />
-
-//             </div>
-//         )
-//     }
-// }

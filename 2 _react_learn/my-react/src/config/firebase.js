f1daa638be2ff32d92ec4545bae75d696738866f
shindekalpesharun <shindekalpesharun@gmail.com>
2022-01-05 22:13:49 +0530

    60 firebase install and confi

---
+++ 2 _react_learn/my-react/src/config/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBvjlTknRY9CCkjEGeA4xsZ1fvss27d1tU",
+    authDomain: "in-depth-react-1-d0750.firebaseapp.com",
+    projectId: "in-depth-react-1-d0750",
+    storageBucket: "in-depth-react-1-d0750.appspot.com",
+    messagingSenderId: "527768525004",
+    appId: "1:527768525004:web:438ee85b8fb3370cec8a69"
+};
+
+// Initialize Firebase
+export default initializeApp(firebaseConfig);
+
+// export default app;
